ab6ae1766c1622d0d7d32a8e0b4342a34515c63c
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-02-11 21:17:13 +0530

    updated the website navbar ui

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
-import "@picocss/pico";
+import '@picocss/pico'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 )
