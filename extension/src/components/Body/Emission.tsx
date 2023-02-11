5623c3027a545a48bdab5b08fb2027b3d5a67131
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-02-12 01:07:02 +0530

    complete the frontend of the extension

--- extension/src/components/Body/Emission.tsx
+++ extension/src/components/Body/Emission.tsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from 'react'
 
 export default function Emission() {
-	const [pageEmission, setPageEmission] = useState(0)
-
+	const [pageEnergy, setPageEnergy] = useState(0)
+	const [pageCO2, setPageCO2] = useState(0)
+	const [pageColor, setPageColor] = useState('')
 	useEffect(() => {
 		fetch('https://api.websitecarbon.com/site?url=https%3A%2F%2Fwww.github.com')
 			.then((res) => res.json())
 			.then((data) => {
 				console.log(data)
-				setPageEmission(data.statistics.energy)
+				setPageEnergy(data.statistics.energy)
+				setPageCO2(data.statistics.co2.grid.grams)
+				if (data.statistics.co2.grid.grams < 1.1) {
+					setPageColor('green')
+				} else if (data.statistics.co2.grid.grams < 2.05) {
+					setPageColor('yellow')
+				} else {
+					setPageColor('red')
+				}
 			})
 			.catch((err) => console.log(err))
 	}, [])
@@ -17,12 +26,17 @@ export default function Emission() {
 		<div className='Emission'>
 			<div className='container-fluid'>
 				<b className=''>Carbon Emissions :</b>
+				<article style={{ backgroundColor: pageColor }}>
+					This page is <span>{pageColor}</span>.
+				</article>
 				<div className='grid'>
 					<article>
 						<header>
 							<span className='small'>Emissions on this page:</span>
 						</header>
-						Energy Consumed: <span style={{ color: 'red' }}>{pageEmission}</span> KW
+						Energy Consumed: <span style={{ color: 'red' }}>{pageEnergy}</span> KW
+						<br />
+						CO2 Emissions: <span style={{ color: 'red' }}>{pageCO2}</span> g
 					</article>
 					<article>
 						<header>
