7cf71c1151b78c1d63b84cc2249738f97da4a899
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-02-11 20:36:24 +0530

    finishing the frontend of the extension

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import Emission from './components/Emission'
 
 function App() {
 	return (
 		<div className='App container'>
 			<Header />
+			<Emission />
 		</div>
 	)
 }
---
+++ extension/src/components/Emission.tsx
@@ -0,0 +1,23 @@
+export default function Emission() {
+	return (
+		<div className='Emission'>
+			<div className='container-fluid'>
+				<h6>Emissions:</h6>
+				<div className='grid'>
+					<article>
+						<header>
+							<span className='small'>Emissions on this page:</span>
+						</header>
+						000
+					</article>
+					<article>
+						<header>
+							<span className='small'>Emissions by you till now:</span>
+						</header>
+						000
+					</article>
+				</div>
+			</div>
+		</div>
+	)
+}
