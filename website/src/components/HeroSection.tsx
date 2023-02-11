a56e6d830eb54343590905bcf4d24f5f20b9bff5
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-02-12 05:06:51 +0530

    Add hero section

--- website/src/components/HeroSection.tsx
+++ website/src/components/HeroSection.tsx
@@ -1,35 +1,104 @@
 const HeroSection = () => {
 	return (
-		<div className="hero">
-			<table>
-				<thead>
-					<tr>
-						<th scope='col'>Rank</th>
-						<th scope='col'>Website</th>
-						
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th scope='row'>1</th>
-						<td>Cell</td>
-					</tr>
-					<tr>
-						<th scope='row'>1</th>
-						<td>Cell</td>
-					</tr>
-					<tr>
-						<th scope='row'>1</th>
-						<td>Cell</td>
-					</tr>
-				</tbody>
-				<tfoot>
-					<tr>
-						<th scope='col'>#</th>
-						<td scope='col'>Total</td>
-					</tr>
-				</tfoot>
-			</table>
+		<div className='hero'>
+			The Worst websites(based on carbon emission in single visit)
+			<div className='container-fluid'>
+				<table>
+					<thead>
+						<tr>
+							<th scope='col'>Rank</th>
+							<th scope='col'>Website</th>
+							<th scope='col'>Link</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope='row'>1</th>
+							<td>DailyMail</td>
+							<td>
+								<a href='https://www.dailymail.co.uk/home/index.html'>Link</a>
+							</td>
+						</tr>
+						<tr>
+							<th scope='row'>2</th>
+							<td>Reddit</td>
+							<td>
+								<a href='https://www.reddit.com/'>Link</a>
+							</td>
+						</tr>
+						<tr>
+							<th scope='row'>3</th>
+							<td>Slack</td>
+							<td>
+								<a href='https://slack.com/intl/en-in'>Link</a>
+							</td>
+						</tr>
+						<tr>
+							<th scope='row'>4</th>
+							<td>Brazil Government</td>
+							<td>
+								<a href='https://www.gov.br/en/government-of-brazil'>Link</a>
+							</td>
+						</tr>
+						<tr>
+							<th scope='row'>5</th>
+							<td>Bath and body Works</td>
+							<td>
+								<a href='https://www.bathandbodyworks.in/'>Link</a>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			The best Websites
+			<div className='container-fluid'>
+				<table>
+					<thead>
+						<tr>
+							<th scope='col'>Rank</th>
+							<th scope='col'>Website</th>
+							<th scope='col'>Link</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope='row'>1</th>
+							<td>Instagram</td>
+							<td>
+								<a href='https://www.instagram.com/'>Link</a>
+							</td>
+						</tr>
+						<tr>
+							<th scope='row'>2</th>
+							<td>Telegraph</td>
+							<td>
+								<a href='https://www.telegraphindia.com/'>Link</a>
+							</td>
+						</tr>
+						<tr>
+							<th scope='row'>3</th>
+							<td>DpBoss</td>
+							<td>
+								<a href='https://dpboss.net/'>Link</a>
+							</td>
+						</tr>
+						<tr>
+							<th scope='row'>4</th>
+							<td>Sogou</td>
+							<td>
+								<a href='https://www.sogou.com/'>Link</a>
+							</td>
+						</tr>
+						<tr>
+							<th scope='row'>5</th>
+							<td>Uk Government</td>
+							<td>
+								<a href='https://www.gov.uk/'>Link</a>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
 	)
 }
