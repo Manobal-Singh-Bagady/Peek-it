ef838cae5523c659b92f34f28d08d42008307e45
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-02-12 06:03:54 +0530

    pretty printing main.tsx

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import '@picocss/pico'
-import {FaGithub,FaHome} from "react-icons/fa"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<div className='container-fluid'>
-		<App />
+			<App />
 		</div>
 	</React.StrictMode>
 )
