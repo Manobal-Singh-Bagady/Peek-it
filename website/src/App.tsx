7f5325de361a57a2c9b55402ac580ab33832d184
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-02-12 06:05:06 +0530

    pretty print App.tsx

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,15 +1,15 @@
-import Navbar from "./components/Navbar"
-import HeroSection from "./components/HeroSection"
-import Footer from "./components/Footer"
-import "./App.css"
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import Footer from './components/Footer'
 
 function App() {
-  
-	return <div className='App'>
-    <Navbar />
-    <HeroSection />
-    <Footer />
-  </div>
+	return (
+		<div className='App'>
+			<Navbar />
+			<HeroSection />
+			<Footer />
+		</div>
+	)
 }
 
 export default App
